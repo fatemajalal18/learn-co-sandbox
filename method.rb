7c87651d95ca5509631a849dc2df39fb97411f27
fatemajalal18 <fatema.w2000@gmail.com>
2018-06-19 14:14:53 +0000

    Done.

---
+++ method.rb
@@ -0,0 +1,20 @@
+#Learn about methods!
+
+# Interpolatian
+
+
+
+def about_me (name,hometown,age = "15",favorite_food = "Pizza")
+  puts "My name is #{name}"
+  puts "I grew up in #{hometown}"
+  puts "I'm #{age} years old"
+  puts "My favorite food is #{favorite_food}"
+end 
+
+about_me("Fatema","Iraq")
+
+
+
+
+
+#create a default argument/ parameter 
